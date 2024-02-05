e89c2ade6893a848b9762fb9380f0e477f057558
seankennelly <sean.kennelly@hotmail.co.uk>
2024-02-05 18:38:50 +0000

    Include favicons

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 
 https://seankennelly.github.io/kreativstorm-RPS-game/
 
+## The Game
+The game is simply Rock, Paper, Scissors. The user can win, lose or draw on each round. After five rounds the scores are added up. Due to the potential for draws in rounds, the final score can also be a win, loss or draw.
+
 ## Scenarios
 The code has been designed to handle the following scenarios:
-* The user's input does not match game rules (ie. 'Rock', 'Paper', 'Scissors')
-* The user's input is not formatted correctly (ie. 'rock', 'paPer')
+* The user's input does not match the intended options (ie. Random strings, numbers, symbols etc)
+* The user's input is not formatted correctly (ie. 'rock', 'paPer' (capitalised incorrectly))
 * The user does not type anything before pressing OK or 'enter'
 * The user's input is correct (program will run as normal)
 
 ## Testing
-Due to the 4 listed scenarios above, four prompts can be reached by the user. There is potential for each scenario to be replayed in each of the four prompts. This has been tested for, with results recorded here:
+In reaction to the four scenarios listed above, four separate prompts can be reached by the user. There is potential for each scenario (correct or incorrect) to then be executed in each of the four prompts. This has been tested for, with results recorded here:
 ![Testing Table](./assets/test_cases.png)
 
--- app.js
+++ app.js
@@ -7,19 +7,18 @@ const computerPlay = () => {
 const playRound = (playerSelection, computerSelection) => {
   const selection = playerSelection;
   if (selection === computerSelection) {
-    return { result: 'draw', msg: `You drew! You both picked ${selection}` }
+    return { result: 'draw', msg: `You drew! You both picked ${selection}.` }
   } else if
     (
     selection === 'Rock' && computerSelection === 'Paper'
     || selection === 'Paper' && computerSelection === 'Scissors'
     || selection === 'Scissors' && computerSelection === 'Rock'
   ) {
-    return { result: 'loss', msg: `You lose! ${computerSelection} beats ${selection}!` }
+    return { result: 'loss', msg: `You lose! The computer's ${computerSelection} beats your ${selection}!` }
   } else {
-    return { result: 'win', msg: `You win! ${selection} beats ${computerSelection}!` }
+    return { result: 'win', msg: `You win! Your ${selection} beats the computer's ${computerSelection}!` }
   };
 };
-
 
 const game = () => {
   let playerScore = 0;
@@ -27,10 +26,10 @@ const game = () => {
   let totalScore = '';
 
   outerLoop: for (let i = 1; i < 6; i++) {
-    let userInput = prompt("1: Please enter either 'Rock', 'Paper', or 'Scissors'. Or type 'Quit' at any time to quit.").toLowerCase();
+    let userInput = prompt("Please enter either 'Rock', 'Paper', or 'Scissors'. Or type 'Quit' at any time to quit.").toLowerCase();
     // CHECKS IF USER HAS ENTERED SOMETHING
     while (userInput === null || userInput.trim() === "") {
-      userInput = prompt("2: Enter either 'Rock', 'Paper', or 'Scissors'. Or type 'Quit' at any time to quit.").toLowerCase();
+      userInput = prompt("Enter either 'Rock', 'Paper', or 'Scissors'. Or type 'Quit' at any time to quit.").toLowerCase();
     };
     // FORMATS INPUT
     let formattedInput = userInput[0].toUpperCase() + userInput.slice(1);
@@ -42,10 +41,10 @@ const game = () => {
     } else {
       // PROMPT FOR INCORRECT INPUT
       while (!playOptions.includes(formattedInput)) {
-        userInput = prompt("3: Sorry, you can only enter 'Rock', 'Paper', or 'Scissors'! (Or 'Quit' to leave the game)").toLowerCase();
+        userInput = prompt("Sorry, you can only enter 'Rock', 'Paper', or 'Scissors'! (Or 'Quit' to leave the game)").toLowerCase();
         // NESTED CHECK FOR EMPTY PROMPT
         while (userInput === null || userInput.trim() === "") {
-          userInput = prompt("4: Enter either 'Rock', 'Paper', or 'Scissors'. Or type 'Quit' at any time to quit.").toLowerCase();
+          userInput = prompt("You must enter 'Rock', 'Paper', or 'Scissors'. Or type 'Quit' at any time to quit.").toLowerCase();
         };
         formattedInput = userInput[0].toUpperCase() + userInput.slice(1);
         // ALLOWS USER TO QUIT FROM SECONDARY PROMPT
@@ -54,9 +53,10 @@ const game = () => {
           break outerLoop;
         };
       };
+      // Player & Computer selections confirmed
       playerSelection = formattedInput;
       const computerSelection = computerPlay();
-
+      // Round played & scores altered (unless draw)
       const round = playRound(playerSelection, computerSelection);
       if (round.result === 'loss') {
         computerScore++;
@@ -66,7 +66,7 @@ const game = () => {
       console.log(`Round ${i}: ${round.msg}`);
     }
   };
-
+  // Final score output
   if (computerScore > playerScore) {
     totalScore = "Sorry, you lost."
   } else if (computerScore < playerScore) {
@@ -74,7 +74,6 @@ const game = () => {
   } else if (computerScore === playerScore) {
     totalScore = "Overall it was a draw.";
   };
-
   console.log(`TOTAL SCORES: 
               Computer Score = ${computerScore}
               Player Score = ${playerScore}
